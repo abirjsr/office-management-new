6e2532c0d7e9cda2daa471617e549b5575c2271d
abirjsr <abirjsr21@gmail.com>
2026-02-22 19:30:16 +0600

    config change

--- next.config.ts
+++ next.config.ts
@@ -14,23 +14,21 @@ const nextConfig = {
       {
         source: '/api/:path*',
         headers: [
-<<<<<<< HEAD
           { key: 'Access-Control-Allow-Origin', value: 'https:office-production-618e.up.railway.app' },
-=======
-          { key: 'Access-Control-Allow-Origin', value: 'https://omb-production-7172.up.railway.app' },
->>>>>>> c0eafc6cb3f0b8f43de09cf96ddc811449a36526
+
+          
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
         ],
       },
     ];
   },
   env: {
-<<<<<<< HEAD
+
     API_BASE_URL: 'https://office-production-618e.up.railway.app', // Update this to your backend URL
-=======
-    API_BASE_URL: 'https://omb-production-7172.up.railway.app',
->>>>>>> c0eafc6cb3f0b8f43de09cf96ddc811449a36526
+  
+
   },
 };
 
 export default nextConfig;
+
